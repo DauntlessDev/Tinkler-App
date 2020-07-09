747b1b52e0810b3e68da818e4884266a1e46ad5a
Brave Leuterio <you@example.com>
2020-07-09 09:28:05 +0800

    added blacktopbg for darkmode

--- lib/ui/widgets/top_background.dart
+++ lib/ui/widgets/top_background.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tinkler/theme/app_theme_service.dart';
 
 class TopBackground extends StatelessWidget {
   const TopBackground({
@@ -10,8 +11,10 @@ class TopBackground extends StatelessWidget {
     return AnimatedOpacity(
       opacity: MediaQuery.of(context).viewInsets.bottom < 50 ? 1.0 : 0.0,
       duration: const Duration(milliseconds: 250),
-      child: const Image(
-        image: const AssetImage('assets/images/top_background.png'),
+      child: Image(
+        image: AppThemeService.isDarkModeOn
+            ? const AssetImage('assets/images/blacktop_background.png')
+            : const AssetImage('assets/images/top_background.png'),
         fit: BoxFit.fill,
         width: double.infinity,
         height: double.infinity,
