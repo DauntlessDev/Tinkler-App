4193bd46ff19d69d7520da89e4980cd93c9a1724
Brave Leuterio <you@example.com>
2020-07-23 10:40:28 +0800

    postbottomsheet ui, finished basic

--- lib/ui/views/home/posts/post_bottomsheet/post_bottomsheet_view.dart
+++ lib/ui/views/home/posts/post_bottomsheet/post_bottomsheet_view.dart
@@ -11,7 +11,7 @@ class PostBottomsheetView extends StatelessWidget {
     return ViewModelBuilder<PostBottomsheetViewModel>.nonReactive(
       builder: (context, model, child) => Container(
         color: Theme.of(context).backgroundColor,
-        height: 350,
+        height: 280,
         width: double.maxFinite,
         child: ClipRRect(
           borderRadius: BorderRadius.circular(15),
@@ -23,11 +23,40 @@ class PostBottomsheetView extends StatelessWidget {
               children: <Widget>[
                 Text('Create Post'),
                 TextField(
+                  minLines: 5,
+                  maxLines: 5,
                   decoration: InputDecoration(
                     hintText: 'Enter status',
                     fillColor: Theme.of(context).colorScheme.onSurface,
                     hintStyle: TextStyle(color: Colors.grey),
                   ),
+                ),
+                SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Container(
+                        color: Theme.of(context).primaryColor,
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Text('Post'),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 6),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Container(
+                        color: Theme.of(context).colorScheme.onSurface,
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Text('Cancel'),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
