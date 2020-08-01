04d61d465d759efba856eecfc082692528357938
Brave Leuterio <you@example.com>
2020-08-01 17:09:33 +0800

    added firebase dep, and pushnotif service class

---
+++ lib/services/functional_services/push_notification_service.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:injectable/injectable.dart';
+
+@lazySingleton
+class PushNotificationService {
+  final FirebaseMessaging _fcm = FirebaseMessaging();
+
+  Future initialise() async {
+    if (Platform.isIOS) {
+      _fcm.requestNotificationPermissions(IosNotificationSettings());
+    }
+
+    _fcm.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+  }
+}
