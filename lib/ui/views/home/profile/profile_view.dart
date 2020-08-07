55e52a9e39cac268ea0ee07cf9d1c793cbbad586
Brave Leuterio <you@example.com>
2020-08-07 20:43:32 +0800

    eddited some in profile

--- lib/ui/views/home/profile/profile_view.dart
+++ lib/ui/views/home/profile/profile_view.dart
@@ -16,12 +16,10 @@ class ProfileView extends StatelessWidget {
     return ViewModelBuilder<ProfileViewModel>.reactive(
       viewModelBuilder: () => ProfileViewModel(),
       builder: (context, model, child) => ModalProgressHUD(
-          inAsyncCall: false,
-          // inAsyncCall: model.profile.email.isEmpty || model.isBusy,
-          child: _MainContent()),
-      // child: model.profile.email.isEmpty
-      //     ? Container(color: Theme.of(context).backgroundColor)
-      //     : _MainContent()),
+          inAsyncCall: model.profile.email.isEmpty || model.isBusy,
+          child: model.profile.email.isEmpty
+              ? Container(color: Theme.of(context).backgroundColor)
+              : _MainContent()),
     );
   }
 }
